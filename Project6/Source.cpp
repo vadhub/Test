ef466ec8ae065b70e5e77f0e41f6ba30a691a0d8
Vadim <gabderahmanov99@gmail.com>
2020-04-27 01:39:01 +0400

    "second programm"

--- Project6/Source.cpp
+++ Project6/Source.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 
-//programm for git
 int main(){
 
 	int a, b;
 	std::cout << "Hello world! Enter two numbers, please."<<endl;
 	std::cin >> a >> b;
 	std::cout << "Summ: " << a + b << endl;
+	std::cout << "Summ: " << a - b << endl;
 
 }
